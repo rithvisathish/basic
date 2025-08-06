49dabbda9e9a8fa219c3517132710423c339d98d
Rithvi <rithvikhaa@live.com>
2025-08-06 12:29:54 +0530

    input function

--- basic.py
+++ basic.py
@@ -1,1 +1,13 @@
-print("hello")
+def leastsquare():
+    x_values = list(map(int, input("Enter all x coordinates separated by commas: ").split(",")))
+    y_values = list(map(int, input("Enter all y coordinates separated by commas: ").split(",")))
+
+    if len(x_values) == len(y_values):
+        print(x_values)
+        print(y_values)
+        
+    else:
+        print("Error. Input again.")
+        leastsquare()
+        
+leastsquare()
